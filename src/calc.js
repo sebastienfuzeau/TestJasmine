68b1ec6a7cc5f4200744003b76f27a7ab4c99edf
sebastienfuzeau <sebastien.fuzeau@espi.fr>
2017-12-08 09:53:47 +0100

    jenkins

--- src/calc.js
+++ src/calc.js
@@ -7,9 +7,40 @@
 **
 ** La fonction renvoie null en cas d'erreur
 */
-function calculatrice(signe, gauche, droite) {
-    var result = null;
 
+
+
+function calculatrice(signe, gauche, droite) {
+
+    var result = null;
+   
+    if (!signe || !gauche || !droite ){
+        return null;
+    }
+    else if (gauche < 0){
+        return null;
+    }
+    else if (droite >= 430){
+        return null;
+    }
+    else if (signe === "+"){
+        result = gauche + droite;
+    }
+    
+    else if (signe === "-"){
+        result = gauche - droite;
+    }
+    
+    else if (signe === "*"){
+        result = gauche * droite;
+    }
+    
+    else if (signe === "/"){
+        result = gauche / droite;
+    }
+    
+    
+    
     return result;
 }
  
